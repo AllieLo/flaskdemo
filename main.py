cb905e9f4512a8d911f92efcb83fdc0109aa470e
jerry <17app001@gmail.com>
2023-04-18 19:52:03 +0800

    新增股市資訊

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
 
+@app.route('/stock')
+def stock():
+    stocks = [
+        {'分類': '日經指數', '指數': '22,920.30'},
+        {'分類': '韓國綜合', '指數': '2,304.59'},
+        {'分類': '香港恆生', '指數': '25,083.71'},
+        {'分類': '上海綜合', '指數': '3,380.68'}
+    ]
+    return render_template('./stock.html', stocks=stocks,
+                           datetime=get_date())
+
+
 @app.route('/index')
 @app.route('/')
 def index():
-    return '<h1>Hello Flask!</h1>'
+    name = 'GUEST'
+    date = get_date()
+    content = {'name': name, 'date': date}
+    return render_template('./index.html', content=content)
 
 
 @app.route('/date')
@@ -15,10 +30,18 @@ def get_date():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
+@app.route('/book')
 @app.route('/book/<int:id>')
-def get_book(id):
-    books = {1: "HTML-Book", 2: "CSS-Book", 3: "JavaScript-Book"}
-    return books[id]
+def get_book(id=None):
+    try:
+        books = {1: "HTML-Book", 2: "CSS-Book", 3: "JavaScript-Book"}
+        if id is not None:
+            return books[id]
+        else:
+            return books
+    except Exception as e:
+        print(e)
+        return '<h2>查詢錯誤，請重新輸入~</h2>'
 
 
 if __name__ == '__main__':
