75f02059afd810065e6cd75dadffc7853f5aeada
jerry <17app001@gmail.com>
2023-04-27 19:13:19 +0800

    暫存修改

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from datetime import datetime
-from pm25 import get_pm25
+from pm25 import get_pm25,get_pm25_db
 import json
 
 app = Flask(__name__)
@@ -28,17 +28,25 @@ def pm25_charts():
 @app.route('/pm25-data', methods=['POST'])
 def get_pm25_data():
     columns, values = get_pm25()
-    # 縣市
-    county = [value[1] for value in values]
-    # 站點名稱
-    site = [value[0] for value in values]
-    # pm2.5數值
-    pm25 = [value[2] for value in values]
-    datas = {
-        'county': county,
-        'site': site,
-        'pm25': pm25
-    }
+
+    datas={'error':'連線錯誤!'}
+    if values is not None:
+        # 縣市
+        county = [value[1] for value in values]
+        # 站點名稱
+        site = [value[0] for value in values]
+        # pm2.5數值
+        pm25 = [value[2] for value in values]
+
+        result=list(zip(site,pm25))
+        result=sorted(datas,key=lambda x:x[-1])
+        print(result)
+   
+        datas = {
+            'county': county,
+            'site': site,
+            'pm25': pm25
+        }
 
     return json.dumps(datas, ensure_ascii=False)
 
@@ -46,15 +54,15 @@ def get_pm25_data():
 @app.route('/pm25', methods=['GET', 'POST'])
 def pm25():
     if request.method == 'GET':
-        columns, values = get_pm25()
+        columns, values = get_pm25_db()
         # 使用GET => request.args.get(name)
         # if request.args.get('sort'):
         #     olumns, values = get_pm25(True)
     if request.method == 'POST':
         if request.form.get('sort'):
-            columns, values = get_pm25(True)
+            columns, values = get_pm25_db(True)
         else:
-            columns, values = get_pm25()
+            columns, values = get_pm25_db()
 
     print(columns, values)
 
--- static/main.js
+++ static/main.js
@@ -1,106 +1,15 @@
 
 const chart = echarts.init(document.querySelector('#main'));
+const pm25HighSite = document.querySelector("#pm25_high_site");
+const pm25HighValue = document.querySelector("#pm25_high_value");
+const pm25LowSite = document.querySelector("#pm25_low_site");
+const pm25LowValue = document.querySelector("#pm25_low_value");
+
+console.log(pm25HighSite, pm25HighValue, pm25LowSite, pm25LowValue);
 
 $(document).ready(() => {
-    drawPM25_1();
+    drawPM25();
 });
-
-function drawPM25_1() {
-    $.ajax(
-        {
-            url: "/pm25-data",
-            type: "POST",
-            dataType: "json",
-            success: (data2) => {
-                console.log(data2);
-                let dataAxis = data2['site'];
-                // prettier-ignore
-                let data = data2['pm25'];
-                let yMax = 500;
-                let dataShadow = [];
-                for (let i = 0; i < data.length; i++) {
-                    dataShadow.push(yMax);
-                }
-                option = {
-                    title: {
-                        text: '特性示例：渐变色 阴影 点击缩放',
-                        subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
-                    },
-                    xAxis: {
-                        data: dataAxis,
-                        axisLabel: {
-                            inside: true,
-                            color: '#fff'
-                        },
-                        axisTick: {
-                            show: false
-                        },
-                        axisLine: {
-                            show: false
-                        },
-                        z: 10
-                    },
-                    yAxis: {
-                        axisLine: {
-                            show: false
-                        },
-                        axisTick: {
-                            show: false
-                        },
-                        axisLabel: {
-                            color: '#999'
-                        }
-                    },
-                    dataZoom: [
-                        {
-                            type: 'inside'
-                        }
-                    ],
-                    series: [
-                        {
-                            type: 'bar',
-                            showBackground: true,
-                            itemStyle: {
-                                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                                    { offset: 0, color: '#83bff6' },
-                                    { offset: 0.5, color: '#188df0' },
-                                    { offset: 1, color: '#188df0' }
-                                ])
-                            },
-                            emphasis: {
-                                itemStyle: {
-                                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                                        { offset: 0, color: '#2378f7' },
-                                        { offset: 0.7, color: '#2378f7' },
-                                        { offset: 1, color: '#83bff6' }
-                                    ])
-                                }
-                            },
-                            data: data
-                        }
-                    ]
-                };
-                // Enable data zoom when user click bar.
-                const zoomSize = 6;
-                chart.on('click', function (params) {
-                    console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
-                    chart.dispatchAction({
-                        type: 'dataZoom',
-                        startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
-                        endValue:
-                            dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
-                    });
-                });
-
-                chart.setOption(option);
-
-            },
-            error: () => {
-                alert("取得資料失敗");
-            }
-        }
-    );
-}
 
 
 function drawPM25() {
