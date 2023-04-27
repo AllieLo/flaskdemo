a5ff1f2f69a81c4f965b3aec86d715b45c4f221e
jerry <17app001@gmail.com>
2023-04-27 20:32:54 +0800

    完成六都繪製

--- static/main.js
+++ static/main.js
@@ -1,5 +1,7 @@
 
 const chart = echarts.init(document.querySelector('#main'));
+const chart1 = echarts.init(document.querySelector('#six'));
+
 const pm25HighSite = document.querySelector("#pm25_high_site");
 const pm25HighValue = document.querySelector("#pm25_high_value");
 const pm25LowSite = document.querySelector("#pm25_low_site");
@@ -9,7 +11,52 @@ console.log(pm25HighSite, pm25HighValue, pm25LowSite, pm25LowValue);
 
 $(document).ready(() => {
     drawPM25();
+    drawSixPM25();
 });
+
+
+function drawSixPM25() {
+    $.ajax(
+        {
+            url: "/pm25-six-data",
+            type: "GET",
+            dataType: "json",
+            success: (data) => {
+                console.log(data);
+                let option = {
+                    title: {
+                        text: ''
+                    },
+                    tooltip: {},
+                    legend: {
+                        textStyle: {
+                            color: 'rgba(200, 205, 255, 0.3)'
+                        },
+                        data: ['PM2.5'],
+                    },
+                    xAxis: {
+                        data: data['county']
+                    },
+                    yAxis: {},
+                    series: [
+                        {
+                            itemStyle: {
+                                color: '#8b008b'
+                            },
+                            name: '數值',
+                            type: 'bar',
+                            data: data['pm25']
+                        }
+                    ]
+                };
+                chart1.setOption(option);
+            },
+            error: () => {
+                alert("取得資料失敗");
+            }
+        }
+    );
+}
 
 
 function drawPM25() {
@@ -40,8 +87,12 @@ function drawPM25() {
                         }
                     ]
                 };
-
                 chart.setOption(option);
+                pm25HighSite.innerText = data['highest'][0];
+                pm25HighValue.innerText = data['highest'][1];
+                pm25LowSite.innerText = data['lowest'][0];
+                pm25LowValue.innerText = data['lowest'][1];
+                $('#date').text(data['date']);
             },
             error: () => {
                 alert("取得資料失敗");
